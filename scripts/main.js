d50bf3fa749c68404a73942809e8cb1b86e740f2
Michael Lajlev <mla@mobilethink.com>
2015-01-03 21:56:14 +0100

    Feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $(function(){
   var t = new Trianglify({x_gradient: Trianglify.colorbrewer.Oranges[9], cellsize: 250, cellpadding: 50, noiseIntensity: 0.35})
-  var pattern = t.generate($('body').width(), $('body').height())
+  var pattern = t.generate($('body').width(), $('.wrapper').height() + 500)
   $('body').css('background-image', pattern.dataUrl)  
 })
