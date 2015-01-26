37e8a7ce6ffc2de314d4e103ab1af8bda91d4e3c
Michael Lajlev <mla@mobilethink.com>
2015-01-26 07:54:59 +0100

    better bg

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(function(){
   var t = new Trianglify({x_gradient: Trianglify.colorbrewer.Oranges[9], cellsize: 250, cellpadding: 50, noiseIntensity: 0.35})
-  var pattern = t.generate($('body').width(), $('.wrapper').height() + 500)
+  // var pattern = t.generate($('body').width(), $('.wrapper').height() + 500)
+  var pattern = t.generate( $('body').width(), $('body').height() )
   $('body').css('background-image', pattern.dataUrl)  
 })
