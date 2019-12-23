cff5ebfd311790972e408f4430291f2bccf01909
Michael Lajlev <lajlev@gmail.com>
2019-12-23 21:51:45 +0100

    initial commit

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * laj.lv
- * https://github.com/lajlev/laj.lv/
- * Copyright (c) 2014 Michael Lajlev
- * Licensed under the MIT license.
- */
-
-exports = module.exports = require('lib/laj.lv');
